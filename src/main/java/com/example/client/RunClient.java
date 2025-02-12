63d8756c8bbda5110eabb115b1867c0d3bb49f4b
Your Name <you@example.com>
2025-02-12 14:56:08 +0100

    Moved client from tests to RunClient

--- src/main/java/com/example/client/RunClient.java
+++ src/main/java/com/example/client/RunClient.java
@@ -1,82 +1,28 @@
 package com.example.client;
 
-import java.io.*;
-import java.net.*;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-import com.example.common.Table;
-import com.example.server.*;
+public class RunClient extends Application{
+  @Override
+  public void start(Stage stage){
+    SceneManager manager = new SceneManager(stage);
 
-public class RunClient {
-  private static final String SERVER_ADDRESS = "localhost";
-  private static final int PORT = 12345;
-
-  public static void main(String[] args) {
-    try (Socket socket = new Socket(SERVER_ADDRESS, PORT);
-         BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
-
-      ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-      ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-
-      AuthenticationData data = new AuthenticationData();
-
-      String mess = (String) in.readObject();
-      System.out.println(mess); // "Podaj login:"
-      data.login = console.readLine();
-      data.password = console.readLine();
-
-      out.writeObject(data);
-      out.flush();
-
-      mess = (String) in.readObject();
-      System.out.println(mess); // "Zalogowano..."
-      mess = (String) in.readObject();
-      System.out.println(mess); // "Podaj nazwę pokoju:"
-
-      out.writeObject(console.readLine());
-      out.flush();
-
-      mess = (String) in.readObject();
-      System.out.println(mess); // "Dołączono do pokoju..."
-
-      final Table[] table = new Table[1];
-
-      new Thread(() -> {
-        try {
-          Object serverMessage;
-          while ((serverMessage = in.readObject()) != null) {
-            if(serverMessage instanceof String s)
-              System.out.println(s);
-            if(serverMessage instanceof Table table1) {
-              table[0] = table1;
-
-              table[0].printPoints();
-              System.out.println("Tyle kart: "+table1.getPlayer(data.login).getNumberOfCards());
-              table[0].getPlayer(data.login).displayHand();
-              System.out.println(table1.getDiscardedDeck().countCards());
-            }
-          }
-        } catch (IOException e) {
-          e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-          throw new RuntimeException(e);
-        }
-      }).start();
-
-      String userInput;
-      while ((userInput = console.readLine()) != null) {
-        try{
-          Integer i = Integer.valueOf(userInput);
-          out.writeObject(i);
-        }catch (Exception ignored){
-          out.writeObject(userInput);
-        }
-        out.flush();
+    stage.setOnCloseRequest((WindowEvent event) -> {
+      try {
+        manager.getNetworkHandler().sendMessage("disconnect");
+      } catch (Exception e) {
       }
+      Platform.exit();
+    });
 
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+    manager.displayScene("login");
   }
+
+  public static void main(String[] args){
+    launch();
+  }
+
 }
